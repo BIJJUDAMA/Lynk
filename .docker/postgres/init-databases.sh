ad4197d42b25132f80c226755536e97bfe623690
Nitansh Shankar <167768158+BIJJUDAMA@users.noreply.github.com>
2026-09-05 17:33:21 +0530

    feat(infra): replace keycloak with supertokens core in docker compose

--- .docker/postgres/init-databases.sh
+++ .docker/postgres/init-databases.sh
@@ -4,7 +4,7 @@ set -e
 function create_database() {
     local database=$1
     echo "Creating database '$database' if not exists..."
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
         SELECT 'CREATE DATABASE $database'
         WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$database')\gexec
 EOSQL
